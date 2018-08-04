4d24d754d711d1b0aa9c48d8f24ec365b4920905
binarization <hi@binarization.com>
2018-08-05 00:12:30 +0800

    DB, Controller, Router

--- server/routes/index.js
+++ server/routes/index.js
@@ -32,4 +32,6 @@ router.get('/message', controllers.message.get)
 // POST 用来处理微信转发过来的客服消息
 router.post('/message', controllers.message.post)
 
+// --- 商品信息接口 Demo --- //
+router.get('/items', controllers.items.list)
 module.exports = router
