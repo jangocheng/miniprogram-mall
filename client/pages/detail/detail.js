9def26d27e818e740617ca6a74b845bb8fa1ac63
binarization <hi@binarization.com>
2018-08-05 23:44:05 +0800

    add: CSS style for detail page
     - [x] item-card
        - [x] item-image
        - [x] item-title
        - [x] item-source
        - [x] item-meta
          - [x] item-currency
          - [x] item-price
     - [x] service-card
        - [x] service-entry
        - [x] service-item
        - [x] service-icon
        - [x] service-text
        - [x] comment-entry
            - [x] comment-button
            - [x] comment-title
            - [x] comment-count
            - [x] comment-arrow
        - [x] comment-preview
     - [x] button-bottom
        - [x] button-add-to-cart
        - [x] button-buy-now
    
    and using `title` as item's title.

--- client/pages/detail/detail.js
+++ client/pages/detail/detail.js
@@ -8,7 +8,7 @@ Page({
         item: {
             id: 1,
             image: 'https://product-1256088332.cos.ap-guangzhou.myqcloud.com/product2.jpg',
-            name: '商品',
+            title: '商品',
             price: 480.5,
             source: '国内·广东'
         },
