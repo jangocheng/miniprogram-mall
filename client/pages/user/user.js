fc39bfffb08fb4b50c8c56e19e1138f4e51fb373
binarization <hi@binarization.com>
2018-08-09 21:48:54 +0800

    add login for page user

--- client/pages/user/user.js
+++ client/pages/user/user.js
@@ -1,11 +1,7 @@
 // pages/user/user.js
 var qcloud = require('../../vendor/wafer2-client-sdk/index')
 var config = require('../../config')
-var util = require('../../utils/util')
-
-const UNPROMPTED = 0
-const UNAUTHORIZED = 1
-const AUTHORIZED = 2
+const app = getApp()
 
 Page({
 
@@ -13,24 +9,14 @@ Page({
      * 页面的初始数据
      */
     data: {
-        userInfo: null,
-        locationAuthType: UNPROMPTED
+        locationAuthType: app.data.locationAuthType
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-        this.checkSession({
-            success: ({
-                userInfo
-            }) => {
-                this.setData({
-                    userInfo: userInfo
-                })
-            },
-            error: () => {}
-        })
+
     },
 
     /**
@@ -44,7 +30,19 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function() {
-
+        // 同步授权状态
+        this.setData({
+            locationAuthType: app.data.locationAuthType
+        })
+        app.checkSession({
+            success: ({
+                userInfo
+            }) => {
+                this.setData({
+                    userInfo
+                })
+            }
+        })
     },
 
     /**
@@ -107,114 +105,19 @@ Page({
      * 用户点击登录按钮
      */
     onTapLogin: function() {
-        this.doQcloudLogin({
+        app.login({
             success: ({
                 userInfo
             }) => {
                 this.setData({
-                    userInfo: userInfo
-                })
-            }
-        })
-    },
-
-    /**
-     * 登录检查
-     */
-    login({
-        success,
-        error
-    }) {
-        wx.getSetting({
-            success: res => {
-                if (res.authSetting['scope.userInfo'] === false) {
-                    this.setData({
-                        locationAuthType: UNAUTHORIZED
-                    })
-                    // 已拒绝授权
-                    wx.showModal({
-                        title: '提示',
-                        content: '请授权我们获取您的用户信息',
-                        showCancel: false
-                    })
-                } else {
-                    this.setData({
-                        locationAuthType: AUTHORIZED
-                    })
-                    this.doQcloudLogin({
-                        success,
-                        error
-                    })
-                }
-            }
-        })
-    },
-
-    /**
-     * 腾讯云登录接口操作
-     */
-    doQcloudLogin: function({
-        success,
-        error
-    }) {
-        qcloud.login({
-            success: res => {
-                if (res) {
-                    let userInfo = res
-                    success && success({
-                        userInfo
-                    })
-                } else {
-                    // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
-                    this.getUserInfo({
-                        success,
-                        error
-                    })
-                }
-            },
-            fail: () => {
-                error && error()
-            }
-        })
-    },
-
-    /**
-     * 检查用户 Session
-     */
-    checkSession: function({success, error }) {
-        wx.checkSession({
-            success: () => {
-                this.getUserInfo({
-                    success,
-                    error
+                    userInfo,
+                    locationAuthType: app.data.locationAuthType
                 })
             },
-            fail: () => {
-                error && error()
-            }
-        })
-    },
-
-    /**
-     * 获取用户信息
-     */
-    getUserInfo: function({ success, error }) {
-        qcloud.request({
-            url: config.service.requestUrl,
-            login: true,
-            success: res => {
-                let data = res.data
-                if (!data.code) {
-                    let userInfo = data.data
-                    success && success({
-                        userInfo
-                    })
-                } else {
-                    error && error()
-                }
-            },
-            fail: () => {
-                error && error()
+            error: () => {
+                this.setData({
+                    locationAuthType: app.data.locationAuthType
+                })
             }
         })
     }
