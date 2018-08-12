bb265168ffec675028c41155b8f10e9f636980ec
binarization <hi@binarization.com>
2018-08-12 22:25:53 +0800

    format user.js

--- client/pages/user/user.js
+++ client/pages/user/user.js
@@ -14,21 +14,21 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
+    onLoad: function (options) {
 
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function() {
+    onReady: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function() {
+    onShow: function () {
         // 同步授权状态
         this.setData({
             locationAuthType: app.data.locationAuthType
@@ -45,35 +45,35 @@ Page({
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function() {
+    onHide: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function() {
+    onUnload: function () {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function() {
+    onReachBottom: function () {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function() {
+    onShareAppMessage: function () {
 
     },
 
@@ -101,7 +101,7 @@ Page({
     /**
      * 用户点击登录按钮
      */
-    onTapLogin: function() {
+    onTapLogin: function () {
         app.login({
             success: ({ userInfo }) => {
                 this.setData({
