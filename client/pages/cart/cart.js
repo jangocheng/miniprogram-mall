85d60883983dbb30902c516563666cc43c926b55
binarization <hi@binarization.com>
2018-08-14 14:00:47 +0800

    modified cart page

--- client/pages/cart/cart.js
+++ client/pages/cart/cart.js
@@ -1,11 +1,35 @@
 // pages/cart/cart.js
+const qcloud = require('../../vendor/wafer2-client-sdk/index')
+const config = require('../../config')
+const app = getApp()
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        userInfo: 1,
+        locationAuthType: app.data.locationAuthType,
+        items: [{
+            id: 1,
+            title: '商品1',
+            image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+            price: 45,
+            source: '海外·瑞典',
+            count: 1,
+        }, {
+            id: 2,
+            title: '商品2',
+            image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
+            price: 158,
+            source: '海外·新西兰',
+            count: 3,
+        }], // 购物车商品列表
+        selectMap: [undefined, true, undefined], // 购物车中选中的id哈希表
+        totalAmount: 45, // 购物车总金额
+        isEdit: false, // 购物车是否处于编辑状态
+        isSelectAll: true, // 购物车中商品是否全选
     },
 
     /**
