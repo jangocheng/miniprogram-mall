365e00b451ec9f2132f511aac101c92c1ed917b0
binarization <hi@binarization.com>
2018-08-14 18:31:21 +0800

    add: onTapLogin function on cart page

--- client/pages/cart/cart.js
+++ client/pages/cart/cart.js
@@ -9,23 +9,9 @@ Page({
      * 页面的初始数据
      */
     data: {
-        userInfo: 1,
+        userInfo: null,
         locationAuthType: app.data.locationAuthType,
-        items: [{
-            id: 1,
-            title: '商品1',
-            image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
-            price: 45,
-            source: '海外·瑞典',
-            count: 1,
-        }, {
-            id: 2,
-            title: '商品2',
-            image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
-            price: 158,
-            source: '海外·新西兰',
-            count: 3,
-        }], // 购物车商品列表
+        items: [], // 购物车商品列表
         selectMap: [undefined, true, undefined], // 购物车中选中的id哈希表
         totalAmount: 45, // 购物车总金额
         isEdit: false, // 购物车是否处于编辑状态
@@ -35,56 +21,75 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
+    onLoad: function (options) {
 
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function() {
+    onReady: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function() {
+    onShow: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function() {
+    onHide: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function() {
+    onUnload: function () {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function() {
+    onReachBottom: function () {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function() {
+    onShareAppMessage: function () {
 
-    }
+    },
+
+    /**
+     * 用户点击登录
+     */
+    onTapLogin: function () {
+        app.login({
+            success: ({ userInfo }) => {
+                this.setData({
+                    userInfo,
+                    locationAuthType: app.data.locationAuthType
+                })
+            },
+            error: () => {
+                this.setData({
+                    locationAuthType: app.data.locationAuthType
+                })
+            }
+        })
+    },
 })
