6c4f38dad6d958ac5b142882b6c8168a0c0fc93e
binarization <hi@binarization.com>
2018-08-14 18:15:06 +0800

    Add item to cart on home page.

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -15,63 +15,63 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
+    onLoad: function (options) {
         this.getItems()
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function() {
+    onReady: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function() {
+    onShow: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function() {
+    onHide: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function() {
+    onUnload: function () {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function() {
+    onReachBottom: function () {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function() {
+    onShareAppMessage: function () {
 
     },
 
     /**
      * 请求商品列表数据
      */
-    getItems: function() {
+    getItems: function () {
         util.showBusy('商品数据加载中...')
         var that = this
         qcloud.request({
@@ -91,5 +91,44 @@ Page({
                 util.showModel('商品数据加载失败', res)
             }
         })
-    }
+    },
+
+    addToCart: function (e) {
+        let item
+        let itemId = e.currentTarget.dataset.id
+        let items = this.data.items
+        for (let i = 0, len = items.length; i < len; ++i) {
+            if (items[i].id === itemId) {
+                item = items[i]
+                break
+            }
+        }
+        if (item) {
+            qcloud.request({
+                url: config.service.cartUrl,
+                login: true,
+                method: 'PUT',
+                data: item,
+                success: result => {
+                    let data = result.data
+                    if (!data.code) {
+                        wx.showToast({
+                            title: '已添加到购物车',
+                        })
+                    } else {
+                        wx.showToast({
+                            icon: 'none',
+                            title: '添加到购物车失败',
+                        })
+                    }
+                },
+                fail: () => {
+                    wx.showToast({
+                        icon: 'none',
+                        title: '添加到购物车失败',
+                    })
+                }
+            })
+        }
+    },
 })
