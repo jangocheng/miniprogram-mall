4b5117797a9196e1f69f69ff12ae1f1d8875c37c
binarization <scm@binarization.com>
2018-08-15 17:22:01 +0800

    add: comment page layout

---
+++ client/pages/comment/comment.js
@@ -0,0 +1,86 @@
+// pages/comment/comment.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        comments: [{
+                avatar: '/images/user.png',
+                username: 'test1',
+                createTime: '2018年01月01日',
+                content: '测试评论',
+            },
+            {
+                avatar: '/images/user.png',
+                username: 'test2',
+                createTime: '2018年02月01日',
+                content: '测试评论',
+            }
+        ], // 评论列表
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        let item = {
+            id: options.id,
+            title: options.title,
+            price: options.price,
+            image: options.image
+        }
+        this.setData({
+            item: item
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
