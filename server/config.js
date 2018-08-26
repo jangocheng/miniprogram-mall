1d932e30ab4eed5d793c9bffb7a4a764d54a0fe4
binarization <scm@binarization.com>
2018-08-26 19:31:46 +0800

    USE SYSTEM ENV

--- server/config.js
+++ server/config.js
@@ -1,12 +1,13 @@
+const ENV = process.env
 const CONF = {
     port: '5757',
     rootPathname: '',
 
     // 微信小程序 App ID
-    appId: '',
+    appId: ENV.APP_ID,
 
     // 微信小程序 App Secret
-    appSecret: '',
+    appSecret: ENV.APP_SECRET,
 
     // 是否使用腾讯云代理登录小程序
     useQcloudLogin: true,
@@ -17,12 +18,12 @@ const CONF = {
      * 开发环境下，MySQL 的初始密码为您的微信小程序 appid
      */
     mysql: {
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        db: 'cAuth',
-        pass: '',
-        char: 'utf8mb4'
+        host: ENV.DB_HOST || 'localhost',
+        port: ENV.DB_PORT || 3306,
+        user: ENV.DB_USER || 'root',
+        db:   ENV.DB_NAME || 'cAuth',
+        pass: ENV.DB_PSWD || '',
+        char: ENV.DB_CHAR || 'utf8mb4'
     },
 
     cos: {
@@ -30,11 +31,11 @@ const CONF = {
          * 地区简称
          * @查看 https://cloud.tencent.com/document/product/436/6224
          */
-        region: 'ap-guangzhou',
+        region: ENV.COS_REGION || 'ap-guangzhou',
         // Bucket 名称
-        fileBucket: 'qcloudtest',
+        fileBucket: ENV.COS_BUCKET || 'qcloudtest',
         // 文件夹
-        uploadFolder: ''
+        uploadFolder: ENV.COS_FOLDER || ''
     },
 
     // 微信登录态有效期
@@ -45,17 +46,17 @@ const CONF = {
      * 其他配置: 用于自行搭建小程序服务
      */
     // 自定义服务器域名
-    // serverHost: '',
+    serverHost: ENV.SERVER_NAME || 'example.com',
     // 腾讯云 Tunnel
-    // tunnelServerUrl: 'https://tunnel.ws.qcloud.la',
+    tunnelServerUrl: ENV.TUNNEL || 'https://tunnel.ws.qcloud.la',
     // 自定义签名钥匙
-    // tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
+    tunnelSignatureKey: ENV.TUNNEL_KEY || '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
     // 腾讯云账号的APPID
-    // qcloudAppId: '',
+    qcloudAppId: ENV.QCLOUD_APP_ID || '',
     // 腾讯云相关配置可以查看云 API 秘钥控制台
     // https://console.cloud.tencent.com/capi
-    // qcloudSecretId: '',
-    // qcloudSecretKey: '',
+    qcloudSecretId: ENV.QCLOUD_SECRET_ID || '',
+    qcloudSecretKey: ENV.QCLOUD_SECRET_KEY || '',
 }
 
 module.exports = CONF
